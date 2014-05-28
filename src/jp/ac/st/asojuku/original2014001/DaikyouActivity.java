4e1cc6501a0d6ef96331d2cd7d024654e1c0abef
student <student@PC111S03.fgakusei.local>
2014-05-28 15:01:10 +0900

    占いfooooooooooooo!!

---
+++ src/jp/ac/st/asojuku/original2014001/DaikyouActivity.java
@@ -0,0 +1,16 @@
+package jp.ac.st.asojuku.original2014001;
+
+import android.os.Bundle;
+
+
+
+public class DaikyouActivity extends MainActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.daikyou_activity);
+
+	}
+}
---
+++ src/jp/ac/st/asojuku/original2014001/kyouActivity.java
@@ -0,0 +1,17 @@
+package jp.ac.st.asojuku.original2014001;
+
+import android.os.Bundle;
+
+
+
+public class kyouActivity extends MainActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.kyou_activity);
+	}
+
+
+}
